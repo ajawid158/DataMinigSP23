a4dcadb8e268c240c0657df9a78db6156dd70df5
ajawid158 <ajawid@auaf.edu.af>
2023-02-12 16:38:02 +0430

    dmn

---
+++ dataManipulation.R
@@ -0,0 +1,78 @@
+
+#data manipulation with dplyr package
+library(dplyr)
+
+#The dataset smoker
+dfTips=read.csv(url('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv'))
+names(dfTips)
+#filter
+fCustomers=filter(dfTips, sex=="Female", smoker=='No')
+head(fCustomers)
+#data manipulation with dplyr package
+#filter 
+#logical operators &, |, !
+unique(dfTips$day)
+weekend=filter(dfTips, day=='Sun'| day=='Sat')
+head(weekend)
+
+#weekend and female
+weekendFemale=filter(dfTips, (day=='Sun'|day=='Sat') & sex=='Female')
+weekendFemale
+
+#Weekdays 
+weekdayCs=filter(dfTips, day != (day=='Sun'|day=='Sat'))
+weekdayCs
+
+#use the function %in%
+unique(dfTips$size)
+
+filter(dfTips, size %in% c(5,6))
+filter(dfTips, day %in% c('Sun','Sat'))
+
+#for numerical >, <, ==
+filter(dfTips, size==4)
+
+#select
+names(dfTips)
+select(dfTips, c(total_bill, tip, size))
+select(dfTips, size, everything())
+select(dfTips, tip:smoker)
+select(dfTips, -(tip:smoker))
+
+#+++++++++++++++++#
+#data manipulation with dplyr package
+#rename
+names(dfTips)
+dfTips1=rename(dfTips, bill=total_bill)
+head(dfTips1)
+
+
+#+++++++++++++++++#
+#data manipulation with dplyr package
+#mutate
+head(dfTips)
+#total spending=total_bill+tip
+head(mutate(dfTips, total_cost=total_bill+tip))
+
+#+++++++++++++++++#
+#data manipulation with dplyr package
+#summarize
+summarise(dfTips, mean(total_bill), mean(tip))
+
+gender=group_by(dfTips, sex)
+summarise(gender, mean(total_bill), sd(total_bill))
+
+
+#+++++++++++++++++#
+#data manipulation with dplyr package
+#pull a column as a vector 
+gender = pull(dfTips, sex)
+
+gender
+
+#+++++++++++++++++#
+#data manipulation with dplyr package
+#sample_n
+dim(dfTips)
+sampledfTips=sample_n(dfTips, 100)
+head(sampledfTips)
