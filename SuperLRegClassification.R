36b66461b8e63b0b9f94ccaf9df74c9aa74aab63
ajawid158 <ajawid@auaf.edu.af>
2023-02-27 12:07:16 +0430

    SupLRegClas

--- SuperLRegClassification.R
+++ SuperLRegClassification.R
@@ -18,9 +18,11 @@ gym_pred=ifelse(m0_pr>0.5, 1, 0)   ##to create a dummy variable
 gym_pred
 x.test$Gymcode
 
-View(x.test)
 table(x.test$Gymcode, gym_pred)   #confussion matrix
-##wrog classifications /6=50%
+##wrog classifications /6=
+plot(x.test$Age, x.test$Gymcode, col='red', pch=16)
+points(x.test$Age, gym_pred, col='green', lwd=3)
+
 
 names(x)
 ###Model 1
@@ -30,17 +32,13 @@ gym_pred1=ifelse(m1_pr>0.5, 1, 0)
 #ms_pred1
 ##plot(x.test$Age, m1_pr)
 table(x.test$Gymcode, gym_pred1)
-###Error rate=1/6 ~ 17%
-x.test$Weight
+###Error rate=
+plot(x.test$Weight, x.test$Gymcode, col='red', pch=16)
+points(x.test$Weight, gym_pred1, col='green', lwd=3)
+
 ###Suppose we choose model 1 how we use this for the new data
 Weight.new=data.frame(Weight=c(60, 90, 85))
 pr_new_data=predict(m1, Weight.new)
 pr_new_data
 cl_new_data=ifelse(pr_new_data>0.5, 1,0)
 cl_new_data
-
-x.fit=fitted(m1, x.train)
-summary(x.fit)
-plot(x.train$Weight, x.fit, col="red", pch=10)
-
-
